766232ffa7cd6b49724c9ad8b99c2fbe3178f22a
Zohaib Ahmed <zohaibahmed1331@gmail.com>
2025-07-11 14:34:14 +0500

    Added port settings to app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
@@ -9,3 +10,7 @@ def home():
 @app.route('/<path:path>')
 def serve_file(path):
     return send_from_directory('hls', path)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # use Railway's assigned port
+    app.run(host='0.0.0.0', port=port)
